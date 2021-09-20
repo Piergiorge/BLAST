67a7c95a52ba43c4bddb04f8b651121ab724a7df
piergiorge <rafaelpiergiorge@gmail.com>
2021-09-20 17:30:23 -0300

    Update blastp_tab.sh

--- protein/blastp_tab.sh
+++ protein/blastp_tab.sh
@@ -1,12 +1,7 @@
-INPUT_DIR=""
-for i in $(ls $INPUT_DIR/*.faa | sed 's/\.faa//g' | sed 's/^.*\///g');
-do
-	makeblastdb -in $INPUT_DIR/$i.faa -dbtype prot -out $i;
-	echo -e 'qseqid\tsseqid\tqstart\tqend\tsstart\tsend\tevalue\tscore' > $i.tab;
-	blastp \
-	-query 28_OBPs.fasta \
-	-db $i \
-	-seg yes \
-	-soft_masking true \
-	-outfmt '6 qseqid sseqid qstart qend sstart send evalue score' >> $i.tab;
-done
+# BLASTP All vs All
+INPUT='sequence.faa'
+OUTPUT='output.tab'
+#dos2unix *;
+makeblastdb -in $INPUT -dbtype prot -out db;
+echo -e 'qseqid\tsseqid\tqstart\tqend\tsstart\tsend\tevalue\tscore' > $OUTPUT;
+blastp -query sequence.faa -num_threads 3 -db db -seg yes -soft_masking true -outfmt '6 qseqid sseqid qstart qend sstart send evalue score' >> $OUTPUT;
