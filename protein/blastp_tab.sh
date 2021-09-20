07008b59d92b0db6d0c4d77e8271d7520e66c7fb
piergiorge <rafaelpiergiorge@gmail.com>
2021-09-20 17:33:38 -0300

    Update blastp_tab.sh

--- protein/blastp_tab.sh
+++ protein/blastp_tab.sh
@@ -3,5 +3,5 @@ INPUT='sequence.faa'
 OUTPUT='output.tab'
 #dos2unix *;
 makeblastdb -in $INPUT -dbtype prot -out db;
-echo -e 'qseqid\tsseqid\tqstart\tqend\tsstart\tsend\tevalue\tscore' > $OUTPUT;
-blastp -query sequence.faa -num_threads 3 -db db -seg yes -soft_masking true -outfmt '6 qseqid sseqid qstart qend sstart send evalue score' >> $OUTPUT;
+echo -e 'qseqid\tsseqid\tpident\tlength\tqstart\tqend\tsstart\tsend\tevalue\tscore' > $OUTPUT;
+blastp -query sequence.faa -num_threads 3 -db db -seg yes -soft_masking true -outfmt '6 qseqid sseqid pident length qstart qend sstart send evalue score' >> $OUTPUT;
